3d73f55706780ac135c1c50072ce1eec0d5bafee
SergeyVorotiy <95651513+SergeyVorotiy@users.noreply.github.com>
2022-09-15 00:47:28 +0300

    Added models
    Created base models
    Migrations have been created and applied

--- bulletinBoard/board/models.py
+++ bulletinBoard/board/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Declaration(models.Model):
+    CATEGORIES = [
+        ('tanks', 'Танки'),
+        ('hills', 'Хилы'),
+        ('DD', 'ДД'),
+        ('merchants', 'Торговцы'),
+        ('gildmasters', 'Гилдмастеры'),
+        ('cvestgivers', 'Квестгиверы'),
+        ('blacksmith', 'Кузнецы'),
+        ('tenners', 'Кожевники'),
+        ('potioncookers', 'Зельевары'),
+        ('spellmasters', 'Мастера заклинаний'),
+    ]
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    text = models.TextField()
+    category = models.CharField(max_length=16, choices=CATEGORIES, default='tanks')
+
+
+class DeclarationResponse(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    declaration = models.ForeignKey(Declaration, on_delete=models.CASCADE)
+    text = models.CharField(max_length=256)
+    accepted = models.BooleanField(default=False)
