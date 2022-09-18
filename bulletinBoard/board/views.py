06825e01187c67ee10d3ff4f75937758af75a3fd
SergeyVorotiy <95651513+SergeyVorotiy@users.noreply.github.com>
2022-09-18 13:30:55 +0300

    save chenged

--- bulletinBoard/board/views.py
+++ bulletinBoard/board/views.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
-from django.contrib.auth.models import User, Permission
+from django.contrib.auth.models import User, Permission, AnonymousUser
 from django.http import HttpResponseRedirect, HttpResponseForbidden, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     DetailView,
@@ -17,12 +18,6 @@ from allauth.account.views import SignupView
 from .forms import ActivationForm, DeclarationForm, DeclarationResponseForm
 from .models import UserActivation, Declaration, DeclarationResponse
 from .filters import ResponseFilter
-
-
-class CustomSignupView(SignupView):
-    def form_valid(self, form):
-        super().form_valid(self, form)
-        return HttpResponseRedirect('/account/activate/')
 
 
 @login_required
@@ -64,11 +59,15 @@ class DeclarationView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['responses'] = DeclarationResponse.objects.filter(declaration=context['declaration'])
+        if self.request.user == context['declaration'].author:
+            context['responses'] = DeclarationResponse.objects.filter(declaration=context['declaration'])
+        elif self.request.user in UserActivation.objects.filter(user_activated=True):
+            context['responses'] = DeclarationResponse.objects.filter(author=self.request.user)
         return context
 
 
-class ResponseView(DetailView):
+class ResponseView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+    permission_required = ('board_adddeclaration', )
     model = DeclarationResponse
     template_name = 'response.html'
     context_object_name = 'response'
@@ -120,6 +119,12 @@ class DeclarationResponseCreateView(LoginRequiredMixin, PermissionRequiredMixin,
         declaration_response.save()
         return HttpResponseRedirect(f'/declaration/{declaration_pk}/')
 
+
+class DeclarationDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    permission_required = ('board.add_declaration', )
+    model = Declaration
+    template_name = 'delete.html'
+    success_url = reverse_lazy('declarations')
 
 class DeclarationUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     permission_required = ('board.add_declaration', )
